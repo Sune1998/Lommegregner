c93e812ce69cb1723db7fc8e009981e4b3140165
Sune1998 <53908735+Sune1998@users.noreply.github.com>
2019-09-16 11:27:05 +0200

    Initial commit

---
+++ src/Lommeregner.java
@@ -0,0 +1,22 @@
+public class Lommeregner {
+    public int sum(int a, int b) {
+        int resultat = a + b;
+        return a + b;
+    }
+int minus (int a , int b) {
+        int resultat = a - b;
+        return resultat;
+    }
+int multi (int a , int b) {
+        int resultat = a * b;
+        return resultat;
+}
+
+int divi (int a , int b) {
+        int resultat = a / b;
+        return resultat;
+}
+
+
+
+    }
